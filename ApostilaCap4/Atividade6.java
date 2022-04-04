b5a1dbe94b26d7ff9aa55d3ed7e59b04f4bbab94
Elizeu <el_vailant@hotmail.com>
2022-04-03 21:53:48 -0300

    Atividade6 -1

--- ApostilaCap4/Atividade6.java
+++ ApostilaCap4/Atividade6.java
@@ -3,5 +3,8 @@ package ApostilaCap4;
 public class Atividade6 {
     public static void main(String[] args) {
         Conta c1 = new Conta();
+        c1.titular = "Abc";
+        Data data = new Data();
+        c1.dataDeAbertura = data;
     }
 }
